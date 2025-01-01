1aa85efdd54f6efc7ac48e7ca6b55e2c0269b0dd
ProjectAhkdan <ahkdanprika1@gmail.com>
2025-01-01 17:58:08 +0700

    dae

--- src/app/home/hero.tsx
+++ src/app/home/hero.tsx
@@ -1,5 +1,6 @@
 // components/Hero.tsx
 import React from "react";
+import Link from "next/link";
 import Image from "next/image";
 import bg from "../../../public/assets/bg.png";
 import { IconYhoikihero } from "../components/icon/icon";
@@ -39,10 +40,10 @@ const Hero: React.FC = () => {
               about technology now? Visit the course menu.
             </p>
           </div>
-         <div className="">
-         <button className="mt-6 px-6 py-3 bg-[linear-gradient(90deg,rgba(117,197,126,1)_0%,rgba(52,168,83,1)_100%)] rounded-full text-white font-medium text-lg hover:opacity-90 transition duration-200">
+         <div className=" pt-8">
+         <Link href="/ContactUs" className="mt-6 px-6 py-3 bg-[linear-gradient(90deg,rgba(117,197,126,1)_0%,rgba(52,168,83,1)_100%)] rounded-full text-white font-medium text-lg hover:opacity-90 transition duration-200">
             Contact us
-          </button>
+          </Link>
          </div>
         </div>
 
