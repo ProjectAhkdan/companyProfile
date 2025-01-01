a0ed2c0c2475a44c144c89f5c7f752b1818ab295
ProjectAhkdan <ahkdanprika1@gmail.com>
2025-01-01 12:46:16 +0700

    icon

---
+++ src/app/components/icon/icon.tsx
@@ -0,0 +1,121 @@
+export const iconCollab = () => {
+  return (
+    <>
+      <svg
+        width="977"
+        height="975"
+        viewBox="0 0 977 975"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M696.919 275.745L626.337 831.975L141.77 354.37L696.919 275.745Z"
+          fill="#D85565"
+        />
+        <path
+          d="M304.748 596.614L276.489 819.309L82.4847 628.092L304.748 596.614Z"
+          fill="#D9D9D9"
+        />
+      </svg>
+    </>
+  );
+};
+
+export const iconSimfly = () => {
+  return (
+    <>
+      <svg
+        width="583"
+        height="590"
+        viewBox="0 0 583 590"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M87.8271 0.699219H495.159V88.225L469.911 133.671L437.93 184.167L404.267 212.781H347.038H202.284L150.105 184.167L109.709 133.671L87.8271 88.225V0.699219Z"
+          fill="#D9D9D9"
+        />
+        <path
+          d="M0.302734 286.848H582.205V411.885L546.137 476.808L500.45 548.944L452.359 589.822H370.604H163.813L89.2713 548.944L31.562 476.808L0.302734 411.885V286.848Z"
+          fill="#D9D9D9"
+        />
+      </svg>
+    </>
+  );
+};
+
+export const iconPeople = () => {
+  return (
+    <>
+      <svg
+        width="657"
+        height="651"
+        viewBox="0 0 657 651"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M263.543 52.0742L491.404 501.267H35.6828L263.543 52.0742Z"
+          fill="#4BA16D"
+        />
+        <path
+          d="M481.783 38.8208L622.887 153.312L472.747 220.306L481.783 38.8208Z"
+          fill="#D9D9D9"
+        />
+      </svg>
+    </>
+  );
+};
+
+export const iconRespect = () => {
+  return (
+    <>
+      <svg
+        width="614"
+        height="536"
+        viewBox="0 0 614 536"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect
+          x="0.493164"
+          y="0.148438"
+          width="216.585"
+          height="535.092"
+          fill="#E39B67"
+        />
+        <rect
+          x="274.41"
+          y="189.031"
+          width="216.585"
+          height="284.554"
+          transform="rotate(-34.2796 274.41 189.031)"
+          fill="#D9D9D9"
+        />
+      </svg>
+    </>
+  );
+};
+
+export const iconAdapt = () => {
+  return (
+    <>
+      <svg
+        width="977"
+        height="975"
+        viewBox="0 0 977 975"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M697.022 275.745L626.439 831.975L141.872 354.37L697.022 275.745Z"
+          fill="#25B6D6"
+        />
+        <path
+          d="M304.85 596.614L276.591 819.309L82.5873 628.092L304.85 596.614Z"
+          fill="#D9D9D9"
+        />
+      </svg>
+    </>
+  );
+};
